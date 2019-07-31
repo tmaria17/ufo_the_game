2d7431e497ea3195be8bef6a89f9dafb19d755ea
Maria Torres <25314178+tmaria17@users.noreply.github.com>
2019-07-31 12:41:04 -0600

    Create a method for evaluating a guess.

--- lib/game.rb
+++ lib/game.rb
@@ -22,6 +22,17 @@ class Game
     puts "Codeword: #{dash_length} "
 
   end
+  
+  def evaluate_guess(guess=nil)
+    if @codeword.chars.include?(guess)
+      @correct_guess_arr.push(guess)
+      "Correct! You're closer to cracking the codeword."
+    elsif
+      @incorrect_guess_arr.push(guess)
+      @incorrect_guess_counter += 1
+      "Incorrect! The tractor beam pulls the person in further."
+    end
+  end
 
   def populate_blank_with_correct_guesses
   @codeword.chars.map do |letter|
